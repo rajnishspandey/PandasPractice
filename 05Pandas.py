28f3ec988c9c0587e47aaec7f2a1e33b5ea90635
rajnishspandey <pandeyrajnish86@yahoo.com>
2023-11-04 01:39:26 +0530

    Practice

--- 05Pandas.py
+++ 05Pandas.py
@@ -7,5 +7,15 @@ import pandas as pd
 data = [{'Pandas': 'dataframe', 'For': 'using', 'Pandas': 'list'},
         {'Pandas':10, 'For': 20, 'Pandas': 30}] 
 
-df = pd.DataFrame(data)
+df = pd.DataFrame(data, index=[1,2])
+print(df)
+
+#using from_reocrds
+print("using from_records")
+df = pd.DataFrame.from_records(data, index=[3,4])
+print(df)
+#using from_dict
+
+print("using from_dict")
+df = pd.DataFrame.from_dict(data)
 print(df)
