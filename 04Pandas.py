c67dc939700e58b55c8b443cb8c7aee07c26b65a
rajnishspandey <pandeyrajnish86@yahoo.com>
2023-11-04 01:31:44 +0530

    Practice

--- 04Pandas.py
+++ 04Pandas.py
@@ -1,4 +1,19 @@
+#Creating a Pandas dataframe using list of tuples
 
 #importing pandas
-
 import pandas as pd
+
+# data in the form of list of tuples
+data = [('Peter', 18, 7),
+        ('Riff', 15, 6),
+        ('John', 17, 8),
+        ('Michel', 18, 7),
+        ('Sheli', 17, 5) ]
+
+df = pd.DataFrame(data, columns=["Name","Ages","Score"])
+print(df)
+
+#using for_records
+print("for records")
+df = pd.DataFrame.from_records(data,columns=["Name","Ages","Score"])
+print(df)
