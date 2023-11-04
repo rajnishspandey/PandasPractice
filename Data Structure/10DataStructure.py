48e2e7d1e2e84db6f213d24d8be7927520868098
rajnishspandey <pandeyrajnish86@yahoo.com>
2023-11-05 02:36:54 +0530

    DataFrame_Practice

--- Data Structure/10DataStructure.py
+++ Data Structure/10DataStructure.py
@@ -4,5 +4,10 @@ Select and display the movies with a rating higher than a certain threshold.
 """
 import pandas as pd
 
-df = pd.read_csv("movies.csv")
+df = pd.read_csv("Data Structure/movies.csv")
+print(df)
+
+threshold = 8
+
+df = df[df["IMDB_Rating"]>threshold]
 print(df)
