8f09e6f9221c2118d01cf53e0cfeae994d06f4e8
rajnishspandey <pandeyrajnish86@yahoo.com>
2023-11-04 23:03:50 +0530

    SeriesPractice

---
+++ Data Structure/02DataStructure.py
@@ -0,0 +1,18 @@
+#Create a Series containing the ages of a group of people and an index of their names. 
+#Select and display the ages of individuals who are older than 30.
+
+import pandas as pd
+
+ages = [35, 28, 42, 31, 45]
+names = ['Alice', 'Bob', 'Charlie', 'David', 'Eve']
+
+df= pd.Series(ages)
+print(df,end="\n\n")
+
+df = pd.Series(ages,index=names)
+print(df[df>30], end="\n\n") #values
+
+print(df>30) #True or False
+
+
+
