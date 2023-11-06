864ef9e2f099a557402a41c82396e9445723da0a
rajnishspandey <pandeyrajnish86@yahoo.com>
2023-11-06 12:07:27 +0530

    IndexingAndSelection_Practice

---
+++ A_02_Indexing_selection/02IndexingAndSelection.py
@@ -0,0 +1,6 @@
+"""
+Load a DataFrame from a CSV file containing sales data with columns "Date," "Product," and "Revenue." 
+Select and display the revenue for a specific product on a particular date using label-based indexing.
+"""
+
+import pandas as pd
