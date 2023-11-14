7f0473719ba8fad0b8581e457f3b81c4ffadf6f0
rajnishspandey <pandeyrajnish86@yahoo.com>
2023-11-14 22:48:27 +0530

    SQLvsPandas

--- B_SQLvsPandas/05SQLvsPandas_sorting.py
+++ B_SQLvsPandas/05SQLvsPandas_sorting.py
@@ -7,3 +7,17 @@ data = {
 }
 
 student = pd.DataFrame(data)
+print(student, end="\n\n")
+
+#sorted by single column - descending order
+sortedVal = student.sort_values(by="ID", ascending=False)
+print(sortedVal, end="\n\n")
+
+#sorted by single column - Ascending order
+sortedVal = student.sort_values(by="ID", ascending=True)
+print(sortedVal, end="\n\n")
+
+#sort by multiple column
+#sorted by single column - descending order
+sortedVal = student.sort_values(by=["Name","ID"])
+print(sortedVal, end="\n\n")
